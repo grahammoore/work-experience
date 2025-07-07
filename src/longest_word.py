b3214a341886effa11f834e300dd0d71611d20a0
Graham Moore <github@cute.org.uk>
2025-07-08 00:21:16 +0100

    longest word

---
+++ src/longest_word.py
@@ -0,0 +1,11 @@
+
+class LongestWord:
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    def longest_word(self):
+        return self.sentence
+
+    def length_of_longest_word(self):
+        return len(self.sentence)
---
+++ tests/longest_word_tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.longest_word import LongestWord
+
+class LongestWordTests(unittest.TestCase):
+
+    def test_long_sentence_1(self):
+        longest_word = LongestWord('a tiny sentence that has words of various sizes')
+        word = longest_word.longest_word();
+        length = longest_word.length_of_longest_word();
+        self.assertEqual(word , 'sentence', 'Is not the longest wordk')
+        self.assertEqual(length , 8, 'Is not the length of the longest work')
+
+    def test_long_sentence_2(self):
+        longest_word = LongestWord('the quick brown fox jumps over the lazy dog')
+        word = longest_word.longest_word();
+        length = longest_word.length_of_longest_word();
+        self.assertEqual(word , 'jumps', 'Is not the longest wordk')
+        self.assertEqual(length , 5, 'Is not the length of the longest work')
+
+if __name__ == '__main__':
+    unittest.main()
